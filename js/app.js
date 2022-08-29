ee07155f57b10d632f032a16ed5a1d5f92b56207
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-08-30 03:53:21 +0600

    added search button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const loadPhones = async() =>{
-    const url =  `https://openapi.programming-hero.com/api/phones?search=iphone`
+const loadPhones = async(searchText) =>{
+    const url =  `https://openapi.programming-hero.com/api/phones?search=iphone?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
     displayPhones(data.data);
@@ -8,15 +8,16 @@ const loadPhones = async() =>{
 const displayPhones = phones =>{
     // console.log(phones);
     const phonesContainer = document.getElementById('phone-container');
-    phones.forEach(phones => {
+    phonesContainer.textContent = '';
+    phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
-        <div class="card">
-                        <img src="${phones.image}" class="card-img-top" alt="...">
+        <div class="card p-4">
+                        <img src="${phone.image}" class="card-img-top" alt="...">
                         <div class="card-body">
-                          <h5 class="card-title">${phones.phone_name}</h5>
-                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          <h5 class="card-title">${phone.phone_name}</h5>
+                          <p class="card-text">${phone.slug}</p>
                         </div>
                       </div>
         `;
@@ -26,4 +27,12 @@ const displayPhones = phones =>{
 
 }
 
-loadPhones();
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText);
+
+  
+});
+
+loadPhones("phone");
